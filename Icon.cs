ae874467a0fe0c000e3b7f64f53735fc85f845ec
Sarah Bryan <bryansar@grinnell.edu>
2024-01-07 15:00:07 -0600

    initial setup

---
+++ Icon.cs
@@ -0,0 +1,17 @@
+using Godot;
+using System;
+
+public partial class Icon : Sprite2D
+{
+	// Called when the node enters the scene tree for the first time.
+	public override void _Ready()
+	{
+		int i = 42;
+	}
+
+	// Called every frame. 'delta' is the elapsed time since the previous frame.
+	public override void _Process(double delta)
+	{
+		int j = 43;
+	}
+}
